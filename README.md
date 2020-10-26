806bafbf0834a29e804d89619acfe59a5db4f7d9
Grzegorz Mikina <gregorimikina@gmail.com>
2020-10-26 23:07:45 +0100

    Load files

---
+++ README.md
@@ -0,0 +1,3 @@
+# randomIntBetweenJS
+
+Losowanie liczb całkowitych z przedziału 4-6 włącznie
---
+++ js/main.js
@@ -0,0 +1,7 @@
+// alert('to działa!');
+
+//Losowanie liczb z przedziału
+
+Math.floor(Math.random()*(6-4+1)+4);
+
+const numberRandomInt =(min,max)=>Math.floor(Math.random()*(max-min+1)+min);
